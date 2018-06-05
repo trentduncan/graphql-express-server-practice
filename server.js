39ba48d7baf38c293f0daf0a17432f7e8527317a
trentduncan <trent.dunc@gmail.com>
2018-06-05 14:14:22 -0700

    trying out some queries

--- server.js
+++ server.js
@@ -6,21 +6,29 @@ const { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
-  type Query {
-    hello: String
+type Query {
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
 `);
 
 // The root provides a resolver function for each API endpoint
 const root = {
-  hello: () => {
-    return 'Hello world!';
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+  },
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
   },
 };
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
-  schema: schema,
+  schema,
   rootValue: root,
   graphiql: true,
 }));
